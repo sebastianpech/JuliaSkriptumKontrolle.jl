b098629a7370b415c32a8400d8e771cf17b9d8e9
Sebastian Pech <sebastian.pech@me.com>
2019-08-22 20:58:51 +0200

    Add linear algebra test example

--- src/Exercises.jl
+++ src/Exercises.jl
@@ -10,4 +10,11 @@
 # 
 #    set_score("excercise identifier",score)
 
+# 11. Lineare Algebra
 
+# 11.3.1 Löse das Gleichungsystem
+
+check_functions["11.3.1"] = function(result)
+    @assert all(result .≈ [-63.24999999999998, 45.833333333333314, 131.0833333333333])
+end
+set_score("11.3.1",1.0)
