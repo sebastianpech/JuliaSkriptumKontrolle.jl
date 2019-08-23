bde87ad6e09ed27e4390eaed3a8b87d80b158f50
Sebastian Pech <sebastian.pech@me.com>
2019-08-23 14:17:48 +0200

    Update assert messages

--- src/Exercises.jl
+++ src/Exercises.jl
@@ -22,20 +22,20 @@ set_score("11.3.1",1.0)
 
 # 11.3.2 Verwende die Funktionen eigvals und eigvecs
 # Result must be a named dict with
-# (λ=eigenval, Φ=eigenvec, A_neu=reconstructed)
+# (eigenwerte=eigenval, eigenvektoren=eigenvec, rekonstruiert=reconstructed)
 
 check_functions["11.3.2"] = function(result)
-    @assert sort(collect(keys(result))) == sort((:λ, :ϕ, :A_neu))
-    @assert result.λ ≈ [-0.33159580731341975 2.7486771373723475 6.58291866994107]
-    @assert result.Φ ≈ [ -0.443127   0.0985892  -0.160077
+    @assert sort(collect(keys(result))) == sort([ :eigenwerte, :eigenvektoren, :rekonstruiert ]) "Die letzte Zeile muss (eigenwerte=..., eigenvektoren=..., rekonstruiert=...) lauten, wobei eigenwerte der Vector aller Eigenwerte, eigenvektoren eine Matrix aller Eigenvektoren und rekonstruiert die rekonstruierte Matrix sein muss."
+    @assert all(isapprox.(result.eigenwerte, [-0.33159580731341975, 2.7486771373723475, 6.58291866994107],atol=1e-4)) "Eigenvektoren falsch berechnet."
+    @assert all(isapprox.(result.eigenvektoren, [ -0.443127   0.0985892  -0.160077
                           0.266015  -0.784562   -0.0893556
-                          0.856081  -0.612162   -0.983052]
+                          0.856081  -0.612162   -0.983052], atol=1e-4)) "Eigenwerte falsch berechnet."
 end
 set_score("11.3.2",1.0)
 
 # 11.3.3 Berechne den Winkel zwischen den Vektoren v1 und v2
 
 check_functions["11.3.3"] = function(result)
-    @assert result ≈ 0.20749622643520305
+    @assert result ≈ 0.20749622643520305 "Winkel falsch berechnet. Der Winkel muss in rad berechnet werden."
 end
 set_score("11.3.3",1.0)
