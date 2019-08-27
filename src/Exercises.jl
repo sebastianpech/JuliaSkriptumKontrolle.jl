cfe2aac33cc5e35ea413396eff9e9bc8d43d3d12
Sebastian Pech <sebastian.pech@me.com>
2019-08-27 16:29:51 +0200

    Fix Linear Algebra Aufgaben

--- src/Exercises.jl
+++ src/Exercises.jl
@@ -26,16 +26,26 @@ set_score("11.3.1",1.0)
 
 check_functions["11.3.2"] = function(result)
     @assert sort(collect(keys(result))) == sort([ :eigenwerte, :eigenvektoren, :rekonstruiert ]) "Die letzte Zeile muss (eigenwerte=..., eigenvektoren=..., rekonstruiert=...) lauten, wobei eigenwerte der Vector aller Eigenwerte, eigenvektoren eine Matrix aller Eigenvektoren und rekonstruiert die rekonstruierte Matrix sein muss."
-    @assert all(isapprox.(result.eigenwerte, [-0.33159580731341975, 2.7486771373723475, 6.58291866994107],atol=1e-4)) "Eigenvektoren falsch berechnet."
+    @assert all(isapprox.(result.eigenwerte, [-0.33159580731341975, 2.7486771373723475, 6.58291866994107],atol=1e-4)) "Eigenwerte falsch berechnet."
     @assert all(isapprox.(result.eigenvektoren, [ -0.443127   0.0985892  -0.160077
-                          0.266015  -0.784562   -0.0893556
-                          0.856081  -0.612162   -0.983052], atol=1e-4)) "Eigenwerte falsch berechnet."
+                                                  0.266015  -0.784562   -0.0893556
+                                                  0.856081  -0.612162   -0.983052], atol=1e-4)) "Eigenvektoren falsch berechnet."
+    @assert all(isapprox.(result.rekonstruiert,[1 -1 1
+                                                    2  3 0
+                                                    10 -0.5 5],atol=1e-4)) "Fehler beim Rekonstruieren der Matrix."
 end
 set_score("11.3.2",1.0)
 
-# 11.3.3 Berechne den Winkel zwischen den Vektoren v1 und v2
+# 11.3.3 Schreibe eine funktion zum Berechne des Winkels zwischen zwei Vektoren v1 und v2
+using LinearAlgebra
 
 check_functions["11.3.3"] = function(result)
-    @assert result ≈ 0.20749622643520305 "Winkel falsch berechnet. Der Winkel muss in rad berechnet werden."
+    function check(v1,v2)
+        eq = result(v1,v2) ≈ rad2deg(acos(v1⋅v2/(norm(v1)*norm(v2))))
+        @assert eq  "Winkel für Vektoren $v1 und $v2 falsch berechnet. Hinweis: Der Winkel muss in rad berechnet werden."
+    end
+    check([1,2,3],[4,5,6])
+    check([3.,1.,9.],[4.,-1.0,-3.])
+    check([1,0,0],[1,0,0])
 end
 set_score("11.3.3",1.0)
