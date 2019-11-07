a4c1a1ce2d10c27d8d9ec3ff013728e1e1fe3641
Sebastian Pech <sebastian.pech@me.com>
2019-11-07 16:05:56 +0100

    Change point distribution

--- src/Exercises.jl
+++ src/Exercises.jl
@@ -33,7 +33,6 @@ check_functions["3.5.1"] = function(result)
         end
     end
 end
-set_score("3.5.1",1.0)
 
 # 7. Funktionen
 # 7.4.1 Dreimal printen
@@ -46,7 +45,6 @@ check_functions["7.4.1"] = function(result)
 	@assert out != ["foofoofoo","zackzackzack"] "Abstand vergessen"
 	@assert out == ["foo foo foo","zack zack zack"]
 end
-set_score("7.4.1",1.0)
 
 # 7.4.2 Verketten
 check_functions["7.4.2"] = function(result)
@@ -55,7 +53,6 @@ check_functions["7.4.2"] = function(result)
 	@assert result(f,g,3) == 3^3+5
 	@assert result(length,g,"foo") == 9
 end
-set_score("7.4.2",1.0)
 
 # 7.4.3 Kreisfläche
 
@@ -63,7 +60,6 @@ check_functions["7.4.3"] = function(result)
 	@assert result(3) == 3^2*π
 	@assert result(5) == 5^2*π
 end
-set_score("7.4.3",1.0)
 
 # 7.4.4 Kreisumfang
 
@@ -71,7 +67,6 @@ check_functions["7.4.4"] = function(result)
 	@assert result(3) == 2*3*π
 	@assert result(5) == 2*5*π
 end
-set_score("7.4.4",1.0)
 
 # 7.4.5 Kreis: Eigenschaften printen
 check_functions["7.4.5"] = function(result)
@@ -88,7 +83,6 @@ check_functions["7.4.5"] = function(result)
         @assert out[i+2] == "U = $(round(2*r*π,digits=2))" "Fehler bei der Ausgabe oder Berechnung von U."
     end
 end
-set_score("7.4.5",1.0)
 
 # 8. Arrays, Dicts, Tuples
 # 8.4.1 Drei Arrays zusammenhängen
@@ -99,7 +93,6 @@ check_functions["8.5.1"] = function(result)
 	@assert result(n1,n2,n3) == [1,2,3,4,5,6]
 	# @assert result(n1,s1,c1) == [1,2,"aa","bb",'a','b']
 end
-set_score("8.5.1",1.0)
 
 # 8.4.2 Inneres eines Arrays
 check_functions["8.5.2"] = function(result)
@@ -110,14 +103,12 @@ check_functions["8.5.2"] = function(result)
 	@assert result(b) == c
 	@assert result(c) == []
 end
-set_score("8.5.2",1.0)
 
 # 8.4.3 Ist das Array sortiert?
 check_functions["8.5.3"] = function(result)
 	@assert result(['s','p','a','m']) == false
 	@assert result(['a','m','p','s']) == true
 end
-set_score("8.5.3",1.0)
 
 # 8.4.4 Anagram-Test
 check_functions["8.5.4"] = function(result)
@@ -134,7 +125,6 @@ check_functions["8.5.4"] = function(result)
         @assert !result(w,wd) "Falsche Ausgabe. Worst ist kein Anagram."
     end
 end
-set_score("8.5.4",1.0)
 
 # 8.4.5 N-tes Element eines Dictionaries ausgeben
 check_functions["8.5.5"] = function(result)
@@ -142,13 +132,11 @@ check_functions["8.5.5"] = function(result)
 	@assert result(d,1) == 4
 	@assert result(d,3) == 19
 end
-set_score("8.5.5",1.0)
 
 # 8.4.6 Tuples zusammenfügen
 check_functions["8.5.6"] = function(result)
 	@assert result(('a','b'),('c','d','e')) == ('a','b','c','d','e')
 end
-set_score("8.5.6",1.0)
 
 # 10. Lineare Algebra
 
@@ -167,14 +155,12 @@ check_functions["10.3.1"] = function(lösbar)
                     2 2],[2, 3])
     @assert lösbar(rand(3,3),rand(3))
 end
-set_score("10.3.1",1.0)
 
 # 10.3.2 Löse das Gleichungsystem
 
 check_functions["10.3.2"] = function(result)
     @assert all(result .≈ [-63.24999999999998, 45.833333333333314, 131.0833333333333])
 end
-set_score("10.3.2",1.0)
 
 
 # 10.3.3 Eigenwerte und Eigenvektoren
@@ -185,7 +171,6 @@ check_functions["10.3.3"] = function(rekonstruiere)
     ϕ = eigvecs.(As)
     @assert all(rekonstruiere.(λ,ϕ) .≈ As) "Fehler beim Rekonstruieren der Matrix."
 end
-set_score("10.3.3",1.0)
 
 # 10.3.3 Schreibe eine funktion zum Berechne des Winkels zwischen zwei Vektoren v1 und v2
 
@@ -200,7 +185,6 @@ check_functions["10.3.4"] = function(result)
     check([3.,1.,9.],[4.,-1.0,-3.])
     check([1,0,0],[1,0,0])
 end
-set_score("10.3.4",1.0)
 
 # 12. Controlflow
 
@@ -218,7 +202,6 @@ check_functions["11.3.1"] = function(result)
     numbers = [rand(1:2:101) for _ in 1:10]
     testit.(numbers,Ref("Ungerade"))
 end
-set_score("11.3.1",1.0)
 
 check_functions["11.3.2"] = function(result)
     inp = ["3","1.1","ggg","9",""]
@@ -230,13 +213,11 @@ check_functions["11.3.2"] = function(result)
         @assert out[i] == l "Erwarte: '$(expected[i])' erhalten: '$(out[i])'"
     end
 end
-set_score("11.3.2",2.0)
 
 check_functions["11.3.3"] = function(result)
     @assert count([floor(result(1000),digits=0) for _ in 1:100] .== 3.0) > 90 "Falsch berechnet"
     @assert count([floor(result(1_000_000),digits=1) for _ in 1:20] .== 3.1) > 15 "Falsch berechnet"
 end
-set_score("11.3.3",3.1)
 
 check_functions["11.3.4"] = function(result)
     function check(f,a,b,expected,fname)
@@ -246,7 +227,6 @@ check_functions["11.3.4"] = function(result)
     check(x->cos(x^3),-2,1,1.78718,"cos(x^3)")
     check(x->(x^2/(1+sin(x))),-1,3,6.84768,"(x^2/(1+sin(x))")
 end
-set_score("11.3.4",3.0)
 
 using Statistics    
 
@@ -257,7 +237,6 @@ check_functions["11.3.5.1"] = function(result)
     @assert !any(inrange.(calcs)) "Zu hohe Genauigkeit bei geringer Anzahl an Iterationen."
     @assert std(calcs) != 0.0 "Keine Änderung bei Erhöhung der Iterationen."
 end
-set_score("11.3.5.1",1.5)
 
 check_functions["11.3.5.2"] = function(result)
     inrange(x) = 0.420 <= x <= 0.423
@@ -265,7 +244,6 @@ check_functions["11.3.5.2"] = function(result)
     calcs = [floor(result(10^n),digits=4) for n in 0:3]
     @assert std(calcs) != 0.0 "Keine Änderung bei Erhöhung der Iterationen."
 end
-set_score("11.3.5.2",1.5)
 
 check_functions["11.3.6"] = function(result)
     function generate_random_sentence(number_of_words)
@@ -287,7 +265,6 @@ check_functions["11.3.6"] = function(result)
     @assert result(t.sep*t.sentence*t.sep*t.sep*t.sentence,t.sep) == [t.sentence, t.sentence] "Fehler bei Aufruf mit '$(t.sep*t.sentence*t.sep*t.sep*t.sentence)' und '$(t.sep)'."
     @assert result(t.sentence,'+') == t.split "Fehler bei Aufruf mit '$(t.sentence)' und '+'."
 end
-set_score("11.3.6",3.0)
 
 check_functions["11.3.7"] = function(bsqrt)
     @donts bsqrt(9) :sqrt
@@ -296,7 +273,6 @@ check_functions["11.3.7"] = function(bsqrt)
         @assert isapprox(bsqrt(n),sqrt(n),atol=1e-5) "Fehler bei der Berechnung von '$n'."
     end
 end
-set_score("11.3.7",2.0)
 
 # 11.3.8 Dictionaries zusammenfügen
 check_functions["11.3.8"] = function(result)
@@ -306,7 +282,6 @@ check_functions["11.3.8"] = function(result)
 	@assert result(d1,d2) == Dict("aa"=>4,"pp"=>1,"dd"=>3,"kk"=>19,"hh"=>5,"gg"=>31)
 	@assert result(d1,d3) == Dict("aa"=>4,"pp"=>10,"dd"=>3,"kk"=>19,"hh"=>5,"gg"=>31)
 end
-set_score("11.3.8",1.0)
 
 # 12. Debugger
 # 12.3.1 ggt
@@ -318,7 +293,6 @@ check_functions["12.3.1"] = function(ggt)
     @assert ggt(0,10) == gcd(0,10)
     @assert ggt(-2304,288) == gcd(-2304,288)
 end
-set_score("12.3.1",1.0)
 
 
 # 13. Spezielle Datentypen
@@ -330,7 +304,6 @@ check_functions["13.3.1"] = function(result)
     @assert Day(d) == Day(12)
     @assert Month(d) == Month(1)
 end
-set_score("13.3.1",2.0)
 
 # 14. FileIO
 # 14.3.1 Survival lager
@@ -450,7 +423,6 @@ check_functions["14.3.1"] = function(read_lagers)
         end
     end
 end
-set_score("14.3.1",3.0)
 
 check_functions["14.3.2"] = function(lager_mit)
     Random.seed!(RandomDevice())
@@ -482,7 +454,6 @@ check_functions["14.3.2"] = function(lager_mit)
         end
     end
 end
-set_score("14.3.2",3.0)
 
 check_functions["14.3.3"] = function(gesamt_bestand)
     Random.seed!(RandomDevice())
@@ -502,4 +473,36 @@ check_functions["14.3.3"] = function(gesamt_bestand)
         @assert beB == total "Falscher Bestand erhoben. Erwarte $total, erhalten $beB"
     end
 end
-set_score("14.3.3",3.0)
+
+# Scores
+
+set_score("3.5.1"   ,1.0)
+set_score("7.4.1"   ,1.0)
+set_score("7.4.2"   ,1.0)
+set_score("7.4.3"   ,1.0)
+set_score("7.4.4"   ,1.0)
+set_score("7.4.5"   ,1.0)
+set_score("8.5.1"   ,1.0)
+set_score("8.5.2"   ,1.0)
+set_score("8.5.3"   ,1.0)
+set_score("8.5.4"   ,1.0)
+set_score("8.5.5"   ,1.0)
+set_score("8.5.6"   ,1.0)
+set_score("10.3.1"  ,2.0)
+set_score("10.3.2"  ,1.0)
+set_score("10.3.3"  ,1.0)
+set_score("10.3.4"  ,1.0)
+set_score("11.3.1"  ,1.0)
+set_score("11.3.2"  ,3.0)
+set_score("11.3.3"  ,3.0)
+set_score("11.3.4"  ,3.0)
+set_score("11.3.5.1",1.5)
+set_score("11.3.5.2",1.5)
+set_score("11.3.6"  ,4.0)
+set_score("11.3.7"  ,2.0)
+set_score("11.3.8"  ,1.0)
+set_score("12.3.1"  ,1.0)
+set_score("13.3.1"  ,2.0)
+set_score("14.3.1"  ,4.0)
+set_score("14.3.2"  ,3.0)
+set_score("14.3.3"  ,3.0)
